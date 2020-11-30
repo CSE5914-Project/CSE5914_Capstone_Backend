edf43a8bd4675d68cbdab15f41a389916cef1fff
Dragogo <dong.760@osu.edu>
2020-11-30 11:18:10 -0500

    Revert "Fix all the bug, back to stable"
    This reverts commit 5dc06cff71bc9e9965901df615e6971bb17f4684.

--- chatbot/chatbot/views.py
+++ chatbot/chatbot/views.py
@@ -152,7 +152,6 @@ class Server():
     # Get response from IBM assistant:
     assistant.create_session()
     robot_response = assistant.ask_assistant(user_answer)
-    print(f"Result from process_user_input:{robot_response}")
     assistant.end_session()
     return robot_response
 
@@ -568,7 +567,6 @@ def get_IBM_response(request):
   # Get response from IBM assistant:
   assistant.create_session()
   robot_response = assistant.ask_assistant(user_answer)
-  print(f"Result from get_IBM_response:{robot_response}")
   assistant.end_session()
   return Response(
     data= {"robotResponse": robot_response}
@@ -621,9 +619,7 @@ def post_answer(request):
           
         # Use IBM assistant to search movie based on the genre keywords 
         user_answer = assistant.ask_assistant(user_answer)
-        print(f"Result from post_answer:{user_answer}")
-        # user_answer = user_answer.capitalize()  # some query preprocessing, so "action" ==> "Action"
-        # print(user_answer)
+        user_answer = user_answer.capitalize()  # some query preprocessing, so "action" ==> "Action"
 
         # Step3: Checking whether the requested genere supported by TMDB. if requested genre doesn't exist, return a error response message
         gener_list = server.get_genre_list()  # Update the genre list in server object
